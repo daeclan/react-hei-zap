c3923697e5f0d0aa4296f391e964f0d0b9fd77a0
Declan Murphy <declxn@gmail.com>
2021-05-27 15:55:52 -0500

    Initial commit.

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { Canvas } from "react-three-fiber";
+import "./styles.css";
+
+export default function App() {
+  return <Canvas style={{ background: "#171717" }}></Canvas>;
+}
